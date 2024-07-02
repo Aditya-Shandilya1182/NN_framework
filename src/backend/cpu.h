06fb44e199bcb628f6ef16ec977f7dd263ad25b8
Maverick18 <adityashandilyap22@gmail.com>
2024-07-02 21:51:59 +0530

    python wrapper initiated

--- src/backend/cpu.h
+++ src/backend/cpu.h
@@ -3,9 +3,10 @@
 
 #include "tensor.h"
 
-void add_tensor_cpu(const Tensor* tensor1, const Tensor* tensor2, Tensor* result);
-void sub_tensor_cpu(const Tensor* tensor1, const Tensor* tensor2, Tensor* result);
-void elementwise_mul_tensor_cpu(const Tensor* tensor1, const Tensor* tensor2, Tensor* result);
-void assign_tensor_cpu(const Tensor* tensor, Tensor* result);
+    void add_tensor_cpu(const Tensor* tensor1, const Tensor* tensor2, Tensor* result);
+    void sub_tensor_cpu(const Tensor* tensor1, const Tensor* tensor2, Tensor* result);
+    void elementwise_mul_tensor_cpu(const Tensor* tensor1, const Tensor* tensor2, Tensor* result);
+    void assign_tensor_cpu(const Tensor* tensor, Tensor* result);
+    void reshape_tensor_cpu(Tensor* tensor, int* new_shape, int new_ndim);
 
 #endif 
--- src/backend/tensor.h
+++ src/backend/tensor.h
@@ -6,11 +6,6 @@
 #include <string.h>
 #include <math.h>
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <math.h>
-
 typedef struct {
     float* data;
     int* shape;
@@ -20,12 +15,15 @@ typedef struct {
     char* device;
 } Tensor;
 
-Tensor* create_tensor(const float* data, const int* shape, int ndim);
-void free_tensor(Tensor* tensor);
-float get_element(const Tensor* tensor, const int* indices);
-Tensor* add_tensor(const Tensor* tensor1, const Tensor* tensor2);
-Tensor* sub_tensor(const Tensor* tensor1, const Tensor* tensor2);
-Tensor* elementwise_mul_tensor(const Tensor* tensor1, const Tensor* tensor2);
-Tensor* assign_tensor(const Tensor* tensor);
-Tensor* reshape_tensor(Tensor* tensor, int* new_shape, int new_ndim);
-#endif 
+extern "C" {
+    Tensor* create_tensor(const float* data, const int* shape, int ndim);
+    void free_tensor(Tensor* tensor);
+    float get_element(const Tensor* tensor, const int* indices);
+    Tensor* add_tensor(const Tensor* tensor1, const Tensor* tensor2);
+    Tensor* sub_tensor(const Tensor* tensor1, const Tensor* tensor2);
+    Tensor* elementwise_mul_tensor(const Tensor* tensor1, const Tensor* tensor2);
+    Tensor* assign_tensor(const Tensor* tensor);
+    Tensor* reshape_tensor(Tensor* tensor, int* new_shape, int new_ndim);
+}
+
+#endif
